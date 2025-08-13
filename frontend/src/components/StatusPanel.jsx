2e1c248834c4700f172afa2550c72be7de6711c4
Amman Kashyap <ammankashyap@Ammans-MacBook-Pro-2.local>
2025-08-13 20:21:09 +0530

    added zustand for state managment

--- frontend/src/components/StatusPanel.jsx
+++ frontend/src/components/StatusPanel.jsx
@@ -1,8 +1,8 @@
-import React from "react";
-import { FaSatellite } from "react-icons/fa6";
+import React from 'react';
+import { FaSatellite } from 'react-icons/fa6';
 
-function StatusPanel({ issData }) {
-  if (!issData) {
+function StatusPanel({ issData, isLoading }) {
+  if (isLoading) {
     return (
       <div className="absolute bottom-4 left-4 z-10 p-4 bg-gray-800 bg-opacity-70 rounded-lg shadow-lg text-white w-64">
         <h2 className="text-lg font-bold mb-2">ISS Status</h2>
@@ -15,31 +15,10 @@ function StatusPanel({ issData }) {
     <div className="absolute bottom-4 left-4 z-10 p-4 bg-gray-800 bg-opacity-70 rounded-lg shadow-lg text-white w-64 font-sans">
       <h2 className="text-lg font-bold mb-2">ISS Status</h2>
       <div className="space-y-1 text-sm">
-        <p>
-          Latitude:{" "}
-          <span className="font-mono float-right">
-            {issData.lat.toFixed(4)}°
-          </span>
-        </p>
-        <p>
-          Longitude:{" "}
-          <span className="font-mono float-right">
-            {issData.lng.toFixed(4)}°
-          </span>
-        </p>
-
-        <p>
-          Altitude:{" "}
-          <span className="font-mono float-right">
-            {issData.alt.toFixed(2)} km
-          </span>
-        </p>
-        <p>
-          Speed:{" "}
-          <span className="font-mono float-right">
-            {issData.vel.toFixed(2)} km/h
-          </span>
-        </p>
+        <p>Latitude: <span className="font-mono float-right">{issData.lat.toFixed(4)}°</span></p>
+        <p>Longitude: <span className="font-mono float-right">{issData.lng.toFixed(4)}°</span></p>
+        <p>Altitude: <span className="font-mono float-right">{issData.alt.toFixed(2)} km</span></p>
+        <p>Speed: <span className="font-mono float-right">{issData.vel.toFixed(2)} km/h</span></p>
       </div>
       <div className="flex items-center mt-3 pt-2 border-t border-gray-600">
         <FaSatellite className="text-green-400" />
