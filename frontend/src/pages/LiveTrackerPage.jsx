c499a97b9af1072629fc86e35107e8ae7e889b55
Amman Kashyap <ammankashyap@Ammans-MacBook-Pro-2.local>
2025-08-13 19:35:59 +0530

    added status panel

--- frontend/src/pages/LiveTrackerPage.jsx
+++ frontend/src/pages/LiveTrackerPage.jsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Scene from '../components/Scene';
+import StatusPanel from '../components/StatusPanel';
 
 function LiveTrackerPage() {
+  const [issData, setIssData] = useState(null);
+
   return (
     <div className="relative w-screen h-screen bg-black">
-      {/* UI Overlay will go here later */}
+
+      <StatusPanel issData={issData} />
       
-      {/* The 3D Scene */}
+
       <div className="absolute inset-0 z-0">
-        <Scene />
+
+        <Scene setIssData={setIssData} />
       </div>
     </div>
   );
