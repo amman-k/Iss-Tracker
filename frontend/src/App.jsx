c0d7491e165016463f3391ed9f6d8f5197384196
Amman Kashyap <ammankashyap@Ammans-MacBook-Pro-2.local>
2025-08-13 21:38:34 +0530

    updated to show landing page on render

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,17 @@
-import react from 'react';
-import './App.css'
-import LiveTrackerPage from './pages/LiveTrackerPage';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './pages/LandingPage.jsx';
+import LiveTrackerPage from './pages/LiveTrackerPage.jsx';
 
 function App() {
-
-
   return (
-    <div>
-      <LiveTrackerPage/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/live" element={<LiveTrackerPage />} />
+      </Routes>
+    </Router>
   );
 }
 
-export default App
+export default App;
