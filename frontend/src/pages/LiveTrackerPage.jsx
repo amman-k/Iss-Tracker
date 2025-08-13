138098175b9b6f8eaaaa2dbc640f8a1175f4db90
Amman Kashyap <ammankashyap@Ammans-MacBook-Pro-2.local>
2025-08-13 21:25:50 +0530

    added header to tracker page

--- frontend/src/pages/LiveTrackerPage.jsx
+++ frontend/src/pages/LiveTrackerPage.jsx
@@ -1,13 +1,12 @@
-import React, { useEffect } from 'react';
-import Scene from '../components/Scene.jsx';
-import StatusPanel from '../components/StatusPanel.jsx';
-import { useIssStore } from '../store/issStore.js';
+import React, { useEffect } from "react";
+import Scene from "../components/Scene.jsx";
+import StatusPanel from "../components/StatusPanel.jsx";
+import { useIssStore } from "../store/issStore.js";
+import Header from "../components/Header.jsx";
 
 function LiveTrackerPage() {
-
   const { issData, isLoading, initialize, disconnect } = useIssStore();
 
-  
   useEffect(() => {
     initialize();
     return () => disconnect();
@@ -15,6 +14,7 @@ function LiveTrackerPage() {
 
   return (
     <div className="relative w-screen h-screen bg-black">
+      <Header />
       <StatusPanel issData={issData} isLoading={isLoading} />
       <div className="absolute inset-0 z-0">
         <Scene />
