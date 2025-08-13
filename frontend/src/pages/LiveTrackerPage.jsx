6e9200d0f499f8c6fadcf9ae1608ba037f580824
Amman Kashyap <ammankashyap@Ammans-MacBook-Pro-2.local>
2025-08-13 20:20:59 +0530

    added zustand for state managment

--- frontend/src/pages/LiveTrackerPage.jsx
+++ frontend/src/pages/LiveTrackerPage.jsx
@@ -1,19 +1,23 @@
-import React, { useState } from 'react';
-import Scene from '../components/Scene';
-import StatusPanel from '../components/StatusPanel';
+import React, { useEffect } from 'react';
+import Scene from '../components/Scene.jsx';
+import StatusPanel from '../components/StatusPanel.jsx';
+import { useIssStore } from '../store/issStore.js';
 
 function LiveTrackerPage() {
-  const [issData, setIssData] = useState(null);
+
+  const { issData, isLoading, initialize, disconnect } = useIssStore();
+
+  
+  useEffect(() => {
+    initialize();
+    return () => disconnect();
+  }, [initialize, disconnect]);
 
   return (
     <div className="relative w-screen h-screen bg-black">
-
-      <StatusPanel issData={issData} />
-      
-
+      <StatusPanel issData={issData} isLoading={isLoading} />
       <div className="absolute inset-0 z-0">
-
-        <Scene setIssData={setIssData} />
+        <Scene />
       </div>
     </div>
   );
