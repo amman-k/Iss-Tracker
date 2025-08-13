1f6ed9d941661b6e331cd4697756cff53ddbfd03
Amman Kashyap <ammankashyap@Ammans-MacBook-Pro-2.local>
2025-08-13 11:07:43 +0530

    created function to call wheretheiss api

---
+++ backend/services/issService.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+import IssPosition from "../models/IssPosition";
+
+const ISS_API_URL = "https://api.wheretheiss.at/v1/satellites/25544";
+
+export const fetchIssLocationAndSave = async () => {
+  try {
+    const response = await axios.get(ISS_API_URL);
+    const { latitude, longitude } = response.data;
+    const newPosition = new IssPosition({
+      latitude,
+      longitude,
+    });
+    await newPosition.save();
+  } catch (error) {
+    console.log("Error fetching or saving ISS location", error.message);
+  }
+};
