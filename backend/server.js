ba390ad955d02a4dbf0cca6c7d8767a8c3ecab97
Amman Kashyap <ammankashyap@Ammans-MacBook-Pro-2.local>
2025-08-13 11:30:20 +0530

    added socket connection

--- backend/server.js
+++ backend/server.js
@@ -10,12 +10,25 @@ dotenv.config();
 
 connectDB();
 
-const FETCH_INTERVAL=5000;
-setInterval(fetchIssLocationAndSave,FETCH_INTERVAL);
-
 const app = express();
 const server = http.createServer(app);
 
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`A user connected with socket id: ${socket.id}`);
+  socket.on("disconnect", () => {
+    console.log(`User disconnected: ${socket.id}`);
+  });
+});
+
+const FETCH_INTERVAL = 5000;
+setInterval(fetchIssLocationAndSave, FETCH_INTERVAL);
 app.use(cors());
 
 const PORT = process.env.PORT || 5001;
