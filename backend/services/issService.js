b40f175775fb63d19f4f0fdc4eeaeacb0020044d
Amman Kashyap <ammankashyap@Ammans-MacBook-Pro-2.local>
2025-08-13 19:06:51 +0530

    fixed emit error

--- backend/services/issService.js
+++ backend/services/issService.js
@@ -7,19 +7,21 @@ export const fetchIssLocationAndSave = async (io) => {
   try {
     const response = await axios.get(ISS_API_URL);
     const { latitude, longitude } = response.data;
+
     const newPosition = new IssPosition({
       latitude,
       longitude,
     });
+
     await newPosition.save();
 
     const locationData = {
       lat: latitude,
-      lon: longitude,
+      lng: longitude,
       timestamp: newPosition.timestamp,
     };
     io.emit("iss-location-update", locationData);
   } catch (error) {
-    console.log("Error fetching or saving ISS location", error.message);
+    console.error("Error fetching or saving ISS location:", error.message);
   }
 };
