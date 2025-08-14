8cc83fe3a7638333bd6a179e59d6f13b494b6305
Amman Kashyap <ammankashyap@Ammans-MacBook-Pro-2.local>
2025-08-14 10:35:55 +0530

    added camera focus feature

--- frontend/src/components/ISS.jsx
+++ frontend/src/components/ISS.jsx
@@ -2,12 +2,11 @@ import React, { useRef } from 'react';
 import { useGLTF } from '@react-three/drei';
 import { useFrame } from '@react-three/fiber';
 
-function ISS({ position }) {
-  
+
+function ISS({ position, onFocus }) { 
   const { scene } = useGLTF('/iss.glb'); 
   const issRef = useRef();
 
- 
   useFrame(() => {
     if (issRef.current) {
       issRef.current.lookAt(0, 0, 0);
@@ -19,11 +18,15 @@ function ISS({ position }) {
       ref={issRef}
       object={scene}
       position={position}
-      scale={0.0615} 
+      scale={0.0615}
+
+      onClick={(event) => {
+        event.stopPropagation(); 
+        onFocus();
+      }}
     />
   );
 }
-
 
 useGLTF.preload('/iss.glb');
 
