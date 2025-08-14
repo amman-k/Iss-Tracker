c5c3d83162ea3daade2e3a37af587102d1257daa
Amman Kashyap <ammankashyap@Ammans-MacBook-Pro-2.local>
2025-08-14 17:36:28 +0530

    added background to landing page

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,19 +1,14 @@
 import React from 'react';
-
 import Header from '../components/Header.jsx';
 import InfoOverlay from '../components/InfoOverlay.jsx';
-import Scene from '../components/Scene.jsx';
+import StaticBackground from '../components/StaticBackground.jsx';
 
 function LandingPage() {
   return (
-    <div className="relative w-screen h-screen bg-black text-white">
+    <div className="relative w-screen h-screen text-white">
       <Header />
       <InfoOverlay />
-      
-      {/* The 3D Scene as a background */}
-      <div className="absolute inset-0 z-0">
-        <Scene />
-      </div>
+      <StaticBackground />
     </div>
   );
 }
